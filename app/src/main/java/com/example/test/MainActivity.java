2b1afed362ce2c92447ccd053b9d02a0a62cb28d
dinhtronghao <dinhtronghao12102002@gmail.com>
2023-11-18 16:44:18 +0700

    tung2phut

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     String tunganlol;
+    String tung2phut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
